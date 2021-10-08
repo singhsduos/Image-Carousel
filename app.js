f98716c360a3b98f55a08b78eeb99328d36227d1
singhsduos <ns9628491678@gmail.com>
2021-10-08 22:41:17 +0530

    caption-added

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+//select all images
 const images = document.querySelectorAll('.carouselImage img');
+const captions = document.querySelectorAll('.carouselImage h2');
 
+//select both buttons
 const prev = document.querySelector('#btn-1');
 const next = document.querySelector('#btn-2');
 
-
+//set the array index '0' of images
 var counter = 0;
 let totalImages = images.length;
+let totalCaptions = captions.length;
 
 
-
+// adding click event listener in buttons
 prev.addEventListener('click', () => {
     if (counter >= 1) {
         counter--;
@@ -32,11 +36,18 @@ next.addEventListener('click', () => {
 
 slideShow(counter);
 
+// function for changing images on clicking buttons
 function slideShow(num) {
 
     for (let image of images) {
         image.style.display = "none";
+
     }
 
+    for (let caption of captions) {
+        caption.style.display = "none";
+
+    }
     images[num].style.display = "block";
+    captions[num].style.display = "block";
 }
